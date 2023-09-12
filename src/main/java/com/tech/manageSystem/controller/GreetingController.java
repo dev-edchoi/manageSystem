699a6cf05edc32046753c5e59dd263a7f365814a
Jinyoung <dev.edchoi@gmail.com>
2023-09-12 17:32:26 +0900

    add library.

--- src/main/java/com/tech/manageSystem/controller/GreetingController.java
+++ src/main/java/com/tech/manageSystem/controller/GreetingController.java
@@ -1,19 +1,21 @@
-package com.tech.manageSystem;
+package com.tech.manageSystem.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.util.concurrent.atomic.AtomicLong;
 
-@RestController
+@Controller
 public class GreetingController {
 
     private static final String template ="Hello, %s";
     private final AtomicLong counter = new AtomicLong();
 
     @GetMapping("/greeting")
-    public Greeting greeting(@RequestParam(value = "name", defaultValue = "world") String name){
-        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
     }
 }
