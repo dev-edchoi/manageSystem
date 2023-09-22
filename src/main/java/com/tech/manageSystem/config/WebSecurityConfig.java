6bc3368340c72da094bbc46d1b69de3c514aa4a1
Jinyoung <dev.edchoi@gmail.com>
2023-09-22 13:39:50 +0900

    로그인 작업

--- src/main/java/com/tech/manageSystem/config/WebSecurityConfig.java
+++ src/main/java/com/tech/manageSystem/config/WebSecurityConfig.java
@@ -45,12 +45,12 @@ public class WebSecurityConfig {
                 .dataSource(dataSource)
                 .passwordEncoder(passwordEncoder())
                 .usersByUsernameQuery("select username, password, enabled "
-                        + "from users "
+                        + "from user "
                         + "where username = ?")
-                .authoritiesByUsernameQuery("select username, name "
+                .authoritiesByUsernameQuery("select u.username, r.name "
                         + "from user_role ur inner join user u on ur.user_id = u.id "
                         + "inner join role r on ur.role_id = r.id "
-                        + "where u.id = ?");
+                        + "where u.username = ?");
     }
 
     @Bean
